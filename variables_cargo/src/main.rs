bf0401441fd6afe1bc5557980065b5f18274fff7
moni-roy <roy.moni.ju.cse@gmail.com>
2020-06-04 22:03:40 +0600

    [Cargo] Immutable/Mutable Variables

---
+++ variables_cargo/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // let x = 10;
+    let mut x = 10; // have to add mut to make mutable variable
+    println!("The value of x = {}", x);
+    x = 12;
+    println!("The value of x = {}", x);
+}
