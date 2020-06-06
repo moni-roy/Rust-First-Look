9d5b89853d9a38134e3c74180368b86088da44c4
moni-roy <roy.moni.ju.cse@gmail.com>
2020-06-06 21:02:23 +0600

    [Project] Guessing Game: A sample project in rust

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,30 @@
+extern crate rand;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Welcome to the guessing game!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Please input your guess..");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        println!("You guessed: {}", guess);
+
+        let guess: u32 = guess.trim().parse().expect("Type a number!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!!"),
+            Ordering::Greater => println!("Too big!!"),
+            Ordering::Equal => {
+                println!("You won!!");
+                break;
+            }
+        }
+    }
+}
